c3ff5672a14e2f51629bc73ce8132f90a550e53d
danial24 <danialiqbal24@gmail.com>
2018-02-04 05:56:13 +0500

    added algolia

--- app/components/container/HomeScreen.js
+++ app/components/container/HomeScreen.js
@@ -1,5 +1,17 @@
 import ContainerBase from './ContainerBase';
 import React from 'react';
+import { InstantSearch, SearchBox, Hits, Highlight, RefinementList, Pagination, CurrentRefinements, ClearAll } from 'react-instantsearch/dom';
+function Product({ hit }) {
+    return (
+        <div>
+            <div style={{ marginTop: '10px' }}>
+                <span className="hit-name">
+                    <Highlight attributeName="name" hit={hit} />
+                </span>
+            </div>
+        </div>
+    );
+};
 
 export default class HomeScreen extends ContainerBase {
 
@@ -16,9 +28,23 @@ export default class HomeScreen extends ContainerBase {
         super.componentWillUnmount();
     }
     render() {
-        return (<div>
-            AA gaya hun mein na
-        </div>);
+        return (
+            <div>
+                <div>
+                    <InstantSearch
+                        appId="01OQ7GEEOR"
+                        apiKey="1b66df875a6a370c8177ecb8473acdb5"
+                        indexName="getstarted_actors"
+                    >
+                        <CurrentRefinements />
+                        <ClearAll />
+                        <SearchBox />
+                        <RefinementList attributeName="rating" />
+                        <Hits hitComponent={Product} />
+                        <Pagination />
+                    </InstantSearch>
+                </div>
+            </div>);
     }
 
 }
